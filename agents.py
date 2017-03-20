7302930330354fa184b1ccdbdae1cc5345258a6d
Bay Gross <bgross17@gmail.com>
2017-03-19 22:58:56 -0400

    baybot untested

--- agents.py
+++ agents.py
@@ -65,3 +65,82 @@ class RandomAgent(BaseAgent):
     # Returns a random card from hand.
     def selectKilledCard(self, playerView):
         return random.choice(playerView.selfstate.cards)
+
+
+
+class BayBot(BaseAgent):
+
+    # ordered preference for cards
+    card_preferences = [
+        "DUKE",
+        "CONTESSA",
+        "CAPTAIN",
+        "ASSASSIN",
+        "AMBASSADOR"
+    ]
+
+    # ordered preference for actions 
+    action_preferences = [
+        "ASSASINATE",
+        "COUP",
+        "EXCHANGE", 
+        "DUKE_MONEY",
+        "STEAL",
+        "INCOME",
+        "FOREIGN_AID"
+    ]
+
+
+    def selectAction(self, playerView):
+
+        # return first legal action by preference
+        action_list = coup.find_eligible_actions(playerView.selfstate)
+        action_names = list(map(lambda x: x.name, action_list))
+        action = [i for i in action_preferences if (i in action_names)][0]
+        action = coup.Action[action]
+
+        # quit here, if no need to target
+        if not action.name in ["ASSASSINATE", "COUP", "STEAL"]:
+            return (action, target)
+
+        #otherwise, proceed with targeting
+
+        # sort players by strength, mapping to tuples w/ original index
+        opps = enumerate(playerView.opponents)
+        opps.sort(key=lambda x: (x[1].cards*10 + x[1].coins)).reverse()
+
+        # default is to attack strongest player
+        target = opps[0][0]
+
+        # TODO: unless stealing, in which case choose strongest player 
+        #       w/ at least 2 coins who has not blocked you before
+       
+        return (action, target)
+
+
+    def selectReaction(self, playerView, actionInfo):
+        reactions = set([act for card in playerView.selfstate.cards for act in coup.available_actions[coup.Role[card.name]]
+                        if isinstance(act, coup.Reaction)])
+
+        if actionInfo[0].name == "STEAL":
+            if coup.Reaction.BLOCK_STEAL in reactions:
+                return True
+            return None
+        elif actionInfo[0].name == "ASSASSINATE":
+            if coup.Reaction.BLOCK_ASSASINATION in reactions:
+                return True
+            return None
+        elif actionInfo[0].name == "FOREIGN_AID":
+            if len(list(filter(lambda x: x.name == 'BLOCK_FOREIGN_AID', reactions))):
+                return True
+            return None
+
+    def selectExchangeCards(self, playerView, cards):
+        ordered_cards = cards.sort(key=lambda x: card_preferences.index(x))
+        return ordered_cards[:len(playerView.selfstate.cards)]   
+
+    # Returns a random card from hand.
+    def selectKilledCard(self, playerView):
+        ordered_cards = playerView.selfstate.cards.sort(key=lambda x: card_preferences.index(x))
+        return ordered_cards[-1]   
+
