aa518a24cd207e927ffb2b77b4f09ebc5bb5de44
Mikayla Thompson <mrt@mikayla.codes>
2017-03-22 18:03:54 -0400

    Some pseudocode, copy cli agent into slack agent

--- slack.py
+++ slack.py
@@ -1,19 +1,50 @@
 import coup
 from agents.bots import *
-# from agents.slack import SlackAgent
+from agents.slack import SlackAgent
 
 import sys
 import os
+from functools import partial
+from collections import namedtuple
 
 from slacker import Slacker
 
+listeners = {}
+
+M = namedtuple('M', 'sender message')
+test_message = M('mikayla', 'Hello, world. this is a fake received message.')
 
 def postToChannel(slack, message):
     print(f"#coup: {message}")
     slack.chat.post_message('#coup', message)
 
-def setup():
+def postToUser(slack, username, message):
+    print(f"@{username}: {message}")
+    slack.chat.post_message(f"@{username}", message, as_user=True, link_names=True)
+
+
+def listener(mdata):
+    # determine if this is a relevant message.
+    # if so, determine who it is for.
+    player = mdata.sender
+    if player in listeners:
+        listeners[player](mdata.message)
+    pass
+
+
+def setup(slack):
     # setup the agents/players
+    players = ['mikayla']
+    agents = []
+    for player in players:
+        # Give each agent a partial for a direct-post function
+        # and a listenerRegistration function
+        def registerListener(listenerFunction):
+            listeners[player] = listenerFunction
+
+        agents.append(SlackAgent(post=partial(postToUser, slack, player),
+                                listenerRegistration=registerListener,
+                                name=player))
     # deal the game
     pass
 
@@ -23,7 +54,6 @@ def postSummaryAndStateToChannel(gameState, summary):
 
 def gameLoop():
     # initialstate
-
     turns = 0
     while len(gameState.players) > 1:
         if (turns > 1000): # shouldn't happen with people
@@ -48,7 +78,9 @@ def gameLoop():
 
 def main():
     slack = Slacker(os.environ['SLACK_API_TOKEN'])
-    postToChannel(slack, 'CoupBot is booting up!')
+    setup(slack)
+    listener(test_message)
+    # postToChannel(slack, 'CoupBot is booting up!')
     pass
 
 
